41cf7ab378133861c4c2d9e3cdb5bb3f8dd66b9d
Keragh <kegh0001@stud.kea.dk>
2023-11-30 09:21:53 +0100

    Update Klub.java

--- src/Klub.java
+++ src/Klub.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Klub {
     ArrayList<Medlem> KlubMedlemmer = new ArrayList<>(); // liste over alle klubmedlemmer
 
     public static void checkListForRestance (ArrayList<Medlem> klubMedlemmer) {
-        klubMedlemmer.sort(Comparator.comparing(Medlem -> Medlem.medlemID)); // sorter liste efter medlemsID fra laveste til højeste
+        klubMedlemmer.sort(Comparator.comparing(Medlem -> Medlem.medlemsID)); // sorter liste efter medlemsID fra laveste til højeste
         int n = 0;
-        for (Medlem medlem : medlemList) { // gennemløber arraylisten, og tjekker hvert objekt for restance > 0
-            if (medlem.restance > 0) {
-                System.out.println("\nNavn:\t  "+medlem.navn+"\nMedlemID: "+medlem.medlemID+"\nRestance: "+medlem.restance);
+        for (Medlem medlem : klubMedlemmer) { // gennemløber arraylisten, og tjekker hvert objekt for restance > 0
+            if (medlem.kontingent > 0) {
+                System.out.println("\nNavn:\t  "+medlem.navn+"\nMedlemID: "+medlem.medlemsID+"\nRestance: "+medlem.kontingent);
                 n++; // counter for totalt antal medlemmer i restance
             }
         }
