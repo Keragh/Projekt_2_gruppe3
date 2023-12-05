3918d80257134c057b17e1a3e3209effd2ce1edc
Poccers <jaha0004@stud.kea.dk>
2023-12-05 04:00:37 +0100

    fjernede ved et uheld ændringer i denne klasse, smider dem tilbage.

--- src/Resultater.java
+++ src/Resultater.java
@@ -1,5 +1,8 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Resultater {
     ArrayList<Resultat> resultatListe = new ArrayList<>();
@@ -11,5 +14,13 @@ public class Resultater {
         resultat.tid = tid;
         resultat.dato = dato;
         resultatListe.add(resultat);
+    }
+    List<Resultat> Findtop5forDiciplin (Disciplin disciplin){
+        // sorterer resulatListe i disciplin og returnere liste af top 5 for disciplin
+        return resultatListe.stream()
+                .sorted(Comparator.comparing(Resultat::getTid))
+                .filter(c -> disciplin.equals(c.getDisciplin()))
+                .limit(5)
+                .collect(Collectors.toList());
     }
 }
