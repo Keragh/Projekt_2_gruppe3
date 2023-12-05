655bdc399c41cf6ea170ad76ce96e537a9a2ad5b
Poccers <jaha0004@stud.kea.dk>
2023-12-05 03:55:12 +0100

    Opdateret resultatklasse, og oprette resultater.

--- src/Klub.java
+++ src/Klub.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Klub {
-    ArrayList<Medlem> KlubMedlemmer = new ArrayList<>(); // liste over alle klubmedlemmer
+    ArrayList<Medlem> KlubMedlemmer = new ArrayList<>();// liste over alle klubmedlemmer
 
+    Klub() {}
     public static void checkListForRestance (ArrayList<Medlem> klubMedlemmer) {
         klubMedlemmer.sort(Comparator.comparing(Medlem -> Medlem.medlemsID)); // sorter liste efter medlemsID fra laveste til højeste
         int n = 0;
@@ -14,5 +18,53 @@ public class Klub {
             }
         }
         System.out.println("\nAntal medlemmer i restance: "+n);
+    }
+    public void soegMedlemID (int medlemID) {
+        Optional<Medlem> fundetMedlem = KlubMedlemmer.stream() // Optional bruges som variable type, for at benytte stream metoderne.
+                .filter(Medlem -> Medlem.getMedlemID().equals(medlemID)) // Dette tillader os at simplet filtrer for medlems ID,
+                .findFirst();                                            // og returnere dette til fundetMedlem. Dette gøres da jeg kun er
+                                                                        // interesseret i et enkelt objekt og ikke en liste.
+
+        if (fundetMedlem.isPresent()) {
+            System.out.println("\nFundet medlem:");
+            System.out.println(fundetMedlem.get()+"\n");
+        }
+        else
+            System.out.println("Intet medlem fundet.");
+    }
+    public void soegMedlemNavn (String medlemNavn) {
+        List<Medlem> fundetMedlemmer = KlubMedlemmer.stream()                   // her oprettes en midlertidig liste til at indeholde
+                .filter(Medlem -> Medlem.getMedlemNavn().equals(medlemNavn))    // de filtrerede medlemmer fra Klubmedlemmer listen.
+                .toList();
+
+        if (!fundetMedlemmer.isEmpty()) {           // hvis medlemmer blev fundet, print. ellers, print ikke.
+        int n = 0;
+            for (Medlem medlem: fundetMedlemmer) {
+                n++;
+                System.out.println(medlem+"\n");
+            }
+            System.out.println("Antal medlemmer som hedder "+medlemNavn+": "+n+"\n");
+        }
+        else
+            System.out.println("\nIntet medlem fundet.\n");
+    }
+
+    public void soegHold (Boolean hold) {
+        List<Medlem> fundetMedlemmer = KlubMedlemmer.stream()
+                .filter(Medlem -> Medlem.getHold().equals(hold))
+                .toList();
+
+        if (!fundetMedlemmer.isEmpty()) {           // samme som ovenstående metode (soegMedlemmer), blot sorteret efter hold i stedet.
+            int n = 0;
+            for (Medlem medlem: fundetMedlemmer) {
+                n++;
+                System.out.println(medlem+"\n");
+            }
+            if (hold) {System.out.println("\nAntal medlemmer af Konkurrenceholdet: "+n);}
+            else if (!hold) {System.out.println("\nAntal medlemmer af Motionsholdet: "+n);}
+            else
+                System.out.println("\nKlubben har ingen medlemmer på nuværende tidspunkt.");
+        }
+
     }
 }
--- src/Medlem.java
+++ src/Medlem.java
@@ -1,30 +1,42 @@
 public class Medlem {
     String navn;
     int alder;
+    boolean hold; // note til selv - true = konkurrencehold, false = motionshold - temp !
     protected static int næsteID = 0;
     protected int medlemsID;
     double kontingent;
 
-
-
     //Constructor
-    public Medlem (String navn, int alder){
-        this.medlemsID = næsteID++;
+    public Medlem (String navn, int alder, boolean hold){
         this.navn = navn;
         this.alder = alder;
+        this.hold = hold;
+        næsteID++;
         medlemsID = næsteID;
     }
-
+    public Integer getMedlemID () {
+        return medlemsID;
+    }
+    public String getMedlemNavn () {
+        return navn;
+    }
+    public Boolean getHold () {
+        return hold;
+    }
+    public String holdNavn () {
+        if (hold) {return "Konkurrencehold";}
+        else return "Motionshold";
+    }
     // toString-metode for at konvert objekter til en string
     @Override
     public String toString() {
-        return "Medlem: \n" + "Navn: " + navn + "\n" + "Alder: " + alder + "\n" +"MedlemID: " + medlemsID + "\n";
+        return "Navn: " + navn + "\n" + "Alder: " + alder + "\n" +"MedlemID: " + medlemsID + "\nHold: "+holdNavn();
     }
 
     // Laver en midlertidig main-metode, opretter 2 eksmepler på nyt medlem for at teste det virker
     public static void main(String[] args) {
-        Medlem medlem1 = new Medlem("John Olsen", 60);
-        Medlem medlem2 = new Medlem("Mariah Carey", 18);
+        Medlem medlem1 = new Medlem("John Olsen", 60, true);
+        Medlem medlem2 = new Medlem("Mariah Carey", 18, false);
 
         System.out.println(medlem1);
         System.out.println(medlem2);
